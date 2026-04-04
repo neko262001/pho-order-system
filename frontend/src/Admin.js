2b2d36b5fbf8731a31d969489ab360d0780e0962
Huy Tran <huytran2602001@gmail.com>
2026-04-04 22:25:34 +0800

    fix admin password final

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -18,16 +18,16 @@ const emptyForm = {
   options: { spicyLevel: false, phoType: false }
 };
 
-import React, { useState, useEffect } from "react";
-
 function Admin() {
-    alert("ADMIN NEW VERSION"); 
+  if (!window.__ADMIN_OK__) {
     const pass = prompt("Nhập mật khẩu admin:");
+
     if (pass !== "0903636778") {
       alert("Sai mật khẩu");
       window.location.href = "/";
       return null;
     }
+
     window.__ADMIN_OK__ = true;
   }
 
